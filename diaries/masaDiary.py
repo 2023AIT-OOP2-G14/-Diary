59538af64dd960970f0241710ed1d735f9156c3c
masa2013 <k22002kk@aitech.ac.jp>
2023-12-14 16:22:03 +0900

    k22002_add_Diary

--- diaries/masaDiary.py
+++ diaries/masaDiary.py
@@ -2,7 +2,7 @@ from diaries.AbstractDiary import AbstractDiary
 class masaDiary(AbstractDiary):
     def get_date(self):
         return "2021-12-14"
-def get_summary(self):
-    return """気になったのを調べるのに集中してしまう癖がある"""
+    def get_summary(self):
+        return """気になったのを調べるのに集中してしまう癖がある"""
     def get_author(self):
         return "masa"
