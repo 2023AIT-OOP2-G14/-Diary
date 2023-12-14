28fd2fe9dd377763fbd12569ab377229782448c6
masa2013 <k22002kk@aitech.ac.jp>
2023-12-14 16:18:35 +0900

    Update masaDiary.py

--- diaries/masaDiary.py
+++ diaries/masaDiary.py
@@ -1,9 +1,8 @@
 from diaries.AbstractDiary import AbstractDiary
 class masaDiary(AbstractDiary):
     def get_date(self):
-        return "2021-12-09"
+        return "2021-12-14"
 def get_summary(self):
-    return """今日はオブジェクト指向プログラミング演習2のグループワーク演習だった。
-この資料を作成するのは、正直しんどかった。 資料に書かれてないことも勝手に伝われば良いのに。と思った。"""
+    return """気になったのを調べるのに集中してしまう癖がある"""
     def get_author(self):
-        return "Nagatani"
+        return "masa"
