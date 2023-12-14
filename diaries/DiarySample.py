bc9eeb3f52bdaedc0cdd662763c87ac324017906
nekonekocatcat <n0k2s0m1+github@gmail.com>
2023-12-14 16:47:51 +0900

    K22095中島瑞希修正

--- diaries/DiarySample.py
+++ diaries/DiarySample.py
@@ -2,10 +2,10 @@ from diaries.AbstractDiary import AbstractDiary
 
 class DiarySample(AbstractDiary):
     def get_date(self):
-        return "2023-12-14"
+        return "2021-12-01"
     
     def get_summary(self):
-        return "すごくねむかった"
+        return "なにもない一日だった"
     
     def get_author(self):
-        return "K22095中島瑞希"
+        return "Sample"
